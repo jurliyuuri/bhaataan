2575354e28a1a31efef98edaf4ba63536ed9424a
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2022-06-25 19:35:05 +0900

    Look up nonunique words

--- compile_corpus.js
+++ compile_corpus.js
@@ -3,4 +3,21 @@ exports.__esModule = true;
 var fs = require("fs");
 var dictionary = JSON.parse(fs.readFileSync('bhat.json', { encoding: 'utf8', flag: 'r' }));
 var dictionary_eng = JSON.parse(fs.readFileSync('bhat-to-eng.json', { encoding: 'utf8', flag: 'r' }));
-console.log(dictionary.words);
+// console.log(dictionary.words);
+console.log(get_duplicates(["1", "2", "1"]));
+console.log(get_duplicates(dictionary.words.map(function (e) { return e.entry.form; })));
+function get_duplicates(arrs) {
+    var counted = arrs.map(function (name) { return ({ count: 1, name: name }); })
+        .reduce(function (result, b) {
+        result.set(b.name, (result.get(b.name) || 0) + b.count);
+        return result;
+    }, new Map());
+    // console.log(counted);
+    var ans = [];
+    counted.forEach(function (value, key) {
+        if (value > 1) {
+            ans.push(key);
+        }
+    });
+    return ans;
+}
--- compile_corpus.ts
+++ compile_corpus.ts
@@ -2,4 +2,23 @@ import * as fs from 'fs'
 
 let dictionary = JSON.parse(fs.readFileSync('bhat.json', { encoding: 'utf8', flag: 'r' }));
 let dictionary_eng = JSON.parse(fs.readFileSync('bhat-to-eng.json', { encoding: 'utf8', flag: 'r' }));
-console.log(dictionary.words);
+// console.log(dictionary.words);
+
+// console.log(get_duplicates(["1", "2", "1"]))
+console.log(get_duplicates(dictionary.words.map((e: { entry: { id: number, form: string } }) => e.entry.form)));
+
+function get_duplicates(arrs: ReadonlyArray<string>) {
+	type B = { count: number, name: string };
+	const counted = arrs.map((name: string) => ({ count: 1, name }))
+		.reduce((result: Map<string, number>, b: B) => {
+			result.set(b.name, (result.get(b.name) || 0) + b.count);
+			return result;
+		}, new Map());
+	// console.log(counted);
+	const ans: string[] = [];
+	counted.forEach((value, key) => {
+		if (value > 1) {ans.push(key);}
+	});
+	
+	return ans;
+}
