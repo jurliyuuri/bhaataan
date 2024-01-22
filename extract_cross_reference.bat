aa17e8cccb4f08cc0a97175f88c2f6da4d2c7263
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2024-01-23 04:00:30 +0900

    ファイル整理

--- extract_cross_reference.bat
+++ extract_cross_reference.bat
@@ -1,13 +1,18 @@
 jq --help > NUL
 if errorlevel 1 goto ERROR
 
-
+REM extract relations from the dictionary
 jq -f cross_reference\extract.jq bhat.json > cross_reference\from-ja.json
-jq -f cross_reference\extract.jq bhat-to-eng.json > cross_reference\bhat-to-eng.json
-jq -f cross_reference\extract.jq bhat-to-lineparine.json > cross_reference\bhat-to-lineparine.json
+jq -f cross_reference\extract.jq bhat-to-eng.json > cross_reference\from-eng_.json
+jq -f cross_reference\extract.jq bhat-to-lineparine.json > cross_reference\from-lineparine_.json
 
-jq -f cross_reference\eng-to-ja.jq cross_reference\bhat-to-eng.json > cross_reference\from-eng.json
-jq -f cross_reference\lineparine-to-ja.jq cross_reference\bhat-to-lineparine.json > cross_reference\from-lineparine.json
+REM translate the labels
+jq -f cross_reference\eng-to-ja.jq cross_reference\from-eng_.json > cross_reference\from-eng.json
+jq -f cross_reference\lineparine-to-ja.jq cross_reference\from-lineparine_.json > cross_reference\from-lineparine.json
+
+REM delete the redundant files
+del cross_reference\from-eng_.json 
+del cross_reference\from-lineparine_.json 
 
 goto EOF 
 :ERROR
