ce66b7989390be09d64166894ca1dc0b10b11491
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2024-09-14 06:43:48 +0900

    Refactor

--- scansion/gen_scansion.ts
+++ scansion/gen_scansion.ts
@@ -1,5 +1,4 @@
-// deno run noun_scansion.ts
-
+#!/usr/bin/env -S deno run --allow-write
 type Dict = {
     words: Word[],
     zpdic: unknown,
@@ -23,33 +22,34 @@ import { to_scansion } from "./to_scansion.ts";
 const dict: Dict = dictionary as Dict;
 const nouns = dict.words.filter(word => word.translations.some(translation => translation.title === "母音幹名詞" || translation.title === "子音幹名詞"));
 
-const nouns_stripped = nouns.map(noun => noun.entry.form);
+((words: Word[], filepath: string) => {
+    const stripped = words.map(w => w.entry.form);
+    const multiword_excluded = stripped.filter(w => !(w.includes(" ") || w.includes("_")));
+    const scansions: [string, string][] = multiword_excluded.map(w => [to_scansion(w), w]);
+    const grouped_by_scansion: [string, string[]][] =
+        [...Map.groupBy(scansions, ([scansion, _]) => scansion)].map(([scansion, words]) => [scansion, words.map(([_, word]) => word)])
+        ;
 
-const single_nouns = nouns_stripped.filter(noun => !(noun.includes(" ") || noun.includes("_")));
-const scansions: [string, string][] = single_nouns.map(w => [to_scansion(w), w]);
-const nouns_grouped_by_scansion: [string, string[]][] =
-    [...Map.groupBy(scansions, ([scansion, _]) => scansion)].map(([scansion, words]) => [scansion, words.map(([_, word]) => word)])
-    ;
+    // order by frequency
+    grouped_by_scansion.sort((a, b) => - a[1].length + b[1].length);
 
-// order by frequency
-nouns_grouped_by_scansion.sort((a, b) => - a[1].length + b[1].length);
-
-Deno.writeTextFileSync("noun_scansion.html", `
+    Deno.writeTextFileSync(filepath, `
 <!DOCTYPE html>
 <meta charset="utf-8">
 Deno を入れ、手動で scansion/gen_scansion.bat を走らせて都度更新すること。
 <h2>まとめ</h2>
 <table cellpadding="5" cellspacing="0" border="1">
     <tr><th>scansion</th><th>数</th><th>単語</th></tr>
-    ${nouns_grouped_by_scansion.map(([scansion, words]) => `<tr><td>${scansion}</td><td>${words.length}</td><td>${words.join(", ")}</td></tr>`).join("\n")}
+    ${grouped_by_scansion.map(([scansion, words]) => `<tr><td>${scansion}</td><td>${words.length}</td><td>${words.join(", ")}</td></tr>`).join("\n")}
 </table>
 <h2>個別</h2>
-${nouns_grouped_by_scansion.map(
-    ([scansion, words]) =>
-        `<h3>${scansion}: ${words.length}</h3>
+${grouped_by_scansion.map(
+        ([scansion, words]) =>
+            `<h3>${scansion}: ${words.length}</h3>
         <ul>${words.map((word) => `<li>${word}</li>`).join("\n")}</ul>`
-).join("\n")
-    }
+    ).join("\n")
+        }
 `);
 
-console.log(nouns_grouped_by_scansion)
+    console.log(grouped_by_scansion)
+})(nouns, "noun_scansion.html");
