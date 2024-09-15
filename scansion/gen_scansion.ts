e6864f54c1d82fadd6071ee381723c4aa4bc6557
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2024-09-15 17:51:48 +0900

    名詞・動詞・副詞・連体詞の scansion を足す

--- scansion/gen_scansion.ts
+++ scansion/gen_scansion.ts
@@ -19,10 +19,8 @@ export type Content = { title: string, text: string };
 
 import dictionary from "../bhat.json" with { type: "json" };
 import { to_scansion } from "./to_scansion.ts";
-const dict: Dict = dictionary as Dict;
-const nouns = dict.words.filter(word => word.translations.some(translation => translation.title === "母音幹名詞" || translation.title === "子音幹名詞"));
 
-((words: Word[], filepath: string) => {
+function gen_scansion(words: Word[], filepath: string, title: string): void {
     const stripped = words.map(w => w.entry.form);
     const multiword_excluded = stripped.filter(w => !(w.includes(" ") || w.includes("_")));
     const scansions: [string, string][] = multiword_excluded.map(w => [to_scansion(w), w]);
@@ -36,6 +34,8 @@ const nouns = dict.words.filter(word => word.translations.some(translation => tr
     Deno.writeTextFileSync(filepath, `
 <!DOCTYPE html>
 <meta charset="utf-8">
+<a href="./index.html">scansion 一覧に戻る</a>
+<h1>${title}</h1>
 Deno を入れ、手動で scansion/gen_scansion.bat を走らせて都度更新すること。
 <h2>まとめ</h2>
 <table cellpadding="5" cellspacing="0" border="1">
@@ -52,4 +52,34 @@ ${grouped_by_scansion.map(
 `);
 
     console.log(grouped_by_scansion)
-})(nouns, "noun_scansion.html");
+};
+
+
+function filter_by_parts_of_speech(dict: Dict, parts_of_speech: string[]): Word[] {
+    return dict.words.filter(word => word.translations.some(translation => parts_of_speech.includes(translation.title)));
+}
+
+const dict: Dict = dictionary as Dict;
+const nouns = filter_by_parts_of_speech(dict, ["母音幹名詞", "子音幹名詞"]);
+const verbs = filter_by_parts_of_speech(dict, ["動詞", "状態動詞", "動作動詞", "瞬間動詞"]);
+const advs = filter_by_parts_of_speech(dict, ["副詞"]);
+const prenominals = filter_by_parts_of_speech(dict, ["連体詞"]);
+
+
+gen_scansion(nouns, "noun_scansion.html", "名詞の scansion");
+gen_scansion(verbs, "verb_scansion.html", "動詞の scansion");
+gen_scansion(advs, "adv_scansion.html", "副詞の scansion");
+gen_scansion(prenominals, "prenominal_scansion.html", "連体詞の scansion");
+
+Deno.writeTextFileSync("index.html", `
+<!DOCTYPE html>
+<meta charset="utf-8">
+<a href="../index.html">トップに戻る</a>
+<h1>scansion 一覧</h1>
+<ul>
+<li><a href="./noun_scansion.html">名詞の scansion</a></li>
+<li><a href="./verb_scansion.html">動詞の scansion</a></li>
+<li><a href="./adv_scansion.html">副詞の scansion</a></li>
+<li><a href="./prenominal_scansion.html">連体詞の scansion</a></li>
+</ul>
+`);
